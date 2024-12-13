45bb8036cf589d85b5c869ea02c29b2f2fd7fd14
stickwithme <67421766+stickwithme@users.noreply.github.com>
2024-12-13 20:09:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
-
 const calculator = document.querySelector('.calculator')
 const display = calculator.querySelector('.calculator__display')
 const buttons = calculator.querySelectorAll('.calculator__button')
-
 
 let currentOperand = ''
 let previousOperand = ''
 let operation = undefined
 
-
 function updateDisplay() {
-  display.innerText = currentOperand
+  display.innerText = currentOperand || '0'
 }
 
-
 function appendNumber(number) {
+ 
   if (number === '.' && currentOperand.includes('.')) return
   currentOperand = currentOperand.toString() + number.toString()
   updateDisplay()
 }
 
-
-function chooseOperation(selectedOperation) {
+function chooseOperation(op) {
   if (currentOperand === '') return
   if (previousOperand !== '') {
     compute()
   }
-  operation = selectedOperation
+  operation = op
   previousOperand = currentOperand
   currentOperand = ''
 }
 
-
 function compute() {
-  let result
+  let computation
   const prev = parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
   if (isNaN(prev) || isNaN(current)) return
   switch (operation) {
     case '+':
-      result = prev + current
+      computation = prev + current
       break
     case '-':
-      result = prev - current
+      computation = prev - current
       break
     case '*':
-      result = prev * current
+      computation = prev * current
       break
     case '/':
-      result = prev / current
+      computation = current !== 0 ? prev / current : 'Error'
       break
     default:
       return
   }
-  currentOperand = result
+  currentOperand = computation.toString()
   operation = undefined
   previousOperand = ''
+  updateDisplay()
 }
-
 
 function clear() {
   currentOperand = ''
@@ -69,16 +64,16 @@ function clear() {
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    const value = button.dataset.value
-    if (value === 'C') {
-      clear()
-    } else if (value === '=') {
-      compute()
-      updateDisplay()
-    } else if (isNaN(value)) {
+    const value = button.getAttribute('data-value')
+    if (button.classList.contains('operator')) {
       chooseOperation(value)
+    } else if (button.classList.contains('calculate')) {
+      compute()
+    } else if (button.classList.contains('clear')) {
+      clear()
     } else {
       appendNumber(value)
     }
   })
 })
+updateDisplay()
